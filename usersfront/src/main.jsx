631f069786a342d9450fabd1fe7ea55aa46bbbb1
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-02 01:12:27 -0500

    refactor: import tenant config provider separately

--- usersfront/src/main.jsx
+++ usersfront/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import './styles/extinguishers-mobile.css'
 import App from './App.jsx'
 import { AuthProvider } from './contexts/AuthContext'
-import { TenantConfigProvider } from './contexts/TenantConfigContext'
+import { TenantConfigProvider } from './contexts/TenantConfigProvider'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
