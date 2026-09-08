ca1201cc0a881cedbeae91ffc3513f6569cba7b1
sebastianbbe1829 <sebastianbbe@gmail.com>
2026-09-08 01:59:41 -0500

    feat: add sales history route

--- usersfront/src/routes/AppRoutes.jsx
+++ usersfront/src/routes/AppRoutes.jsx
@@ -23,6 +23,7 @@ import InventoryTypesPage from '../pages/InventoryTypesPage'
 import InventoryProductsPage from '../pages/InventoryProductsPage'
 import InventoryMovementsPage from '../pages/InventoryMovementsPage'
 import SalesPage from '../pages/SalesPage'
+import SalesHistoryPage from '../pages/SalesHistoryPage'
 import TenantAdminPage from '../pages/TenantAdminPage'
 import GlobalSuperAdminPage from '../pages/GlobalSuperAdminPage'
 import TenantConfigPage from '../pages/TenantConfigPage'
@@ -75,6 +76,7 @@ function AppRoutes() {
       <Route path="inventarios/productos" element={<RutaConPermiso permission="INVENTORY_READ"><InventoryProductsPage /></RutaConPermiso>} />
       <Route path="inventarios/movimientos" element={<RutaConPermiso permission="INVENTORY_MOVEMENT_READ"><InventoryMovementsPage /></RutaConPermiso>} />
       <Route path="ventas" element={<RutaConPermiso permission="SALES_READ"><SalesPage /></RutaConPermiso>} />
+      <Route path="ventas/consulta" element={<RutaConPermiso permission="SALES_READ"><SalesHistoryPage /></RutaConPermiso>} />
       <Route path="configuracion-ui" element={<TenantConfigPage />} />
       <Route path="administracion-tenant" element={<TenantAdminPage />} />
       <Route path="usuarios-super" element={<GlobalSuperAdminPage />} />
